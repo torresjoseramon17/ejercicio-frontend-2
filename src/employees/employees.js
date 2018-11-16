a2f9f960f76e869c4553effe4877c4838592fae4
Jose <torresjoseramon17@gmail.com>
2018-11-16 17:59:32 -0600

    cree un compomente llamado employees, y empleaando la funcion map para que me regrese la informacion de los empleados

---
+++ src/employees/employees.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import employees from "./index.js"
+
+class Average extends Component {
+  render() {
+    console.log(employees)
+    return (
+        <div className="container">
+        
+               
+    <table className="table">
+      <thead>
+        <tr>
+          <th>age </th>
+          <th>name</th>
+          <th>company</th>
+           <th>Salary </th>
+           <th>age </th>
+           <th>phone </th>
+           <th>email </th>
+        </tr>
+        </thead>
+        { employees.map(obj=>
+        
+        <tbody>
+          <tr><td>{obj.name}</td>
+          <td>{obj.company}</td>
+          <td>{obj.age}</td>
+          <td>{obj.phone}</td>
+          <td>{obj.email}</td>
+          </tr></tbody>
+        )}
+      
+      
+    </table>
+    </div>
+    );
+  }
+}
+
+export default Average; 
